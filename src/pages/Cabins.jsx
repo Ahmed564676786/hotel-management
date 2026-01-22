69f87a769042b6fc6b4c979a8afdf3bd61d32afa
Ahmed564676786 <ahmedking4660@gmail.com>
2026-01-22 05:16:19 +0500

    error handling

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,13 +2,20 @@ import { useEffect, useState } from 'react';
 import { getCabins, deleteCabin,insertCabin} from '../services/apiCabins';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
-import { Form, useForm } from 'react-hook-form';
+import {useForm } from 'react-hook-form';
 
 function Cabins() {
 
 
-   const {register,handleSubmit}  = useForm();
 
+   const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues
+  } = useForm();
+
+   const queryClient =  useQueryClient();
 
    const {isLoading:creatingCabin,mutate:mutate2} = useMutation({
 
@@ -17,6 +24,10 @@ function Cabins() {
           onSuccess:() => {
 
               toast.success('Cabin Inserted');
+
+              queryClient.invalidateQueries({}
+                
+              );
           },
 
           onError: (err) => toast.error(err.message)
@@ -32,8 +43,6 @@ function Cabins() {
 
    });
 
-
-  const queryClient =  useQueryClient();
 
    const {isLoading:isDeleting,mutate} =  useMutation({
 
@@ -62,11 +71,124 @@ function Cabins() {
 
 
 
+
   return (
 
 
-     <div>
+     <div className='flex flex-col  lg:flex-col'>
 
+
+      <div >
+
+         <div className=" bg-gray-700   p-6 text-white">
+            <div className="  gap-y-4  bg-blue-900 border border-black py-4 px-4">
+              
+
+
+              <form onSubmit={handleSubmit(mutate2)} className="space-y-2">
+              
+                  
+                  <div className="flex  mb-2 gap-2">
+                    <label className="w-24 text-right">Name:</label>
+  
+                    <div className='flex flex-col w-full'>
+                                        <input
+                      name="name"
+                      {...register("name",{
+
+                         required:"This field is required"
+                      })}
+                      type="text"
+                      placeholder="Name"
+                      className="flex-1 rounded border px-2 py-1 text-black"
+                    />
+
+                       {errors.name && <span>{errors.name.message}</span>}
+                    </div>
+                   
+                  </div>
+
+
+                    <div className="flex items-center mb-2 gap-2">
+                      <label className=" text-right">Max Capacity</label>
+                      <div className='flex flex-col '>
+                        <input
+                          name="maxCapacity"
+                          {...register("maxCapacity",{
+                              required:"this is required",
+
+                              min:
+                              {
+                                value:1, 
+                                message:"Capacity should be atleast 1"
+                              }
+                              ,
+                              max:{
+
+                                value:4, 
+                                message:"At most 4"
+                              }
+                          })}
+                          type="number"
+                          placeholder="Max Capacity"
+                          className=" flex-1 text-black rounded border px-2 py-1 "
+                        />
+
+                          {errors.maxCapacity && <span>{errors.maxCapacity.message}</span>}
+                      </div>
+                    </div>
+
+                   <div className="flex items-center mb-2 gap-2">
+  <label className="w-24 text-right">Min Capacity:</label>
+  <input
+    name="minCapacity"
+    {...register("minCapacity")}
+    type="number"
+    placeholder="Min Capacity"
+    className="flex-1 rounded border px-2 py-1 text-black"
+  />
+</div>
+
+<div className="flex items-center mb-2 gap-2">
+  <label className="w-24 text-right">Price:</label>
+  <input
+    name="price"
+    {...register("price")}
+    type="number"
+    placeholder="Price"
+    className="flex-1 rounded border px-2 py-1 text-black"
+  />
+</div>
+
+<div className="flex items-center mb-2 gap-2">
+  <label className="w-24 text-right">Discount:</label>
+  <div>
+    <input  defaultValue={0}
+      id='discount'
+      name="discount"
+      {...register("discount",{
+          required:"this is required",
+          validate:(value) => ( value < getValues().price || "Discount should less than 5")
+      })}
+      type="number"
+      placeholder="Discount"
+      className="flex-1 rounded border px-2 py-1 text-black"
+    />
+     {errors.discount && <span>{errors.discount.message}</span>}
+  </div>
+</div>
+
+    <div className='flex gap-2 '>
+
+         <button type='reset' className=' rounded-lg bg-blue-500 px-4 py-1 text-white'>Reset</button>
+         <button type="submit" className=" rounded-lg bg-blue-500 px-4 py-1 text-white">Submit</button>
+    </div>
+
+    </form>
+            </div>
+          </div>
+      </div>
+      <div>
         <ul className='text-white bg-gray-700 px-2 ' >
 
           <li className="flex justify-between gap-5 py-4">
@@ -106,67 +228,9 @@ function Cabins() {
           }
             
         </ul>
+</div>
 
-            <div className=" bg-gray-700 p-6 text-white">
-            <div className="grid grid-cols-[150px_1fr] gap-y-4 items-center border border-black">
-              
-
-
-              <form onSubmit={handleSubmit(mutate2)} className="space-y-2">
-
-      <label>Name</label>
-      <input
-        name="name"
-        {...register("name")}
-        type="text"
-        placeholder="Name"
-        className="w-1/2 rounded border px-2 py-1 text-black"
-      />
-
-      <label>Max Capacity</label>
-      <input
-        name="maxCapacity"
-        {...register("maxCapacity")}
-        type="number"
-        placeholder="Max Capacity"
-        className="w-1/2 rounded border px-2 py-1 text-black"
-      />
-
-      <label>Min Capacity</label>
-      <input
-        name="minCapacity"
-        {...register("minCapacity")}
-        type="number"
-        placeholder="Min Capacity"
-        className="w-1/2 rounded border px-2 py-1 text-black"
-      />
-
-      <label>Price</label>
-      <input
-        name="price"
-        {...register("price")}
-        type="number"
-        placeholder="Price"
-        className="w-1/2 rounded border px-2 py-1 text-black"
-      />
-
-      <label>Discount</label>
-      <input
-        name="discount"
-        {...register("discount")}
-        type="number"
-        placeholder="Discount"
-        className="w-1/2 rounded border px-2 py-1 text-black"
-      />
-
-   
-
-      <button type="submit" className="mt-2 rounded bg-blue-500 px-4 py-1 text-white">
-        Submit
-      </button>
-    </form>
-            </div>
-          </div>
+           
 
               </div>
   );
